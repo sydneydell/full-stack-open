c9530bf6951786c9b7c42d65831a017db45aae73
Sydney Dell <sadell@mit.edu>
2024-10-21 16:03:55 -0400

    completed exercise 2.10 - refacted app into three separate components

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,46 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
+
+// Component for filtering persons
+const Filter = ({ newSearch, handleSearch }) => (
+  <div>
+    filter shown with <input value={newSearch} onChange={handleSearch} />
+  </div>
+);
+
+// Component for the form to add a new person
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => (
+  <form onSubmit={addPerson}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+
+// Component to display the list of persons
+const Persons = ({ personsToShow }) => (
+  <ul>
+    {personsToShow.map(person => (
+      <li key={person.id}>
+        {person.name} {person.number}
+      </li>
+    ))}
+  </ul>
+);
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 }
-  ])  
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
@@ -47,23 +84,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input value={newSearch} onChange={handleSearch}/></div>
+      <Filter newSearch={newSearch} handleSearch={handleSearch}/>
 
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>name: <input value={newName} onChange={handleNameChange}/></div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-        <div> <button type="submit">add</button></div>
-      </form>
+      <h2>Add a new</h2>
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
 
       <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person => 
-          <li key={person.id}>
-            {person.name} {person.number}
-          </li>
-        )}
-      </ul>
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 }
