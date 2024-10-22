f45fa0ffe297c7a6ae4b3f5768a5e6d98988ffcc
Sydney Dell <sadell@mit.edu>
2024-10-22 13:09:04 -0400

    completed exericse 2.11 - store phonebook data in db.json and application now fetches data from the server using axios library

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 // Component for filtering persons
 const Filter = ({ newSearch, handleSearch }) => (
@@ -35,15 +36,21 @@ const Persons = ({ personsToShow }) => (
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
