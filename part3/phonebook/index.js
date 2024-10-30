e84e24abc0bae1c462dac86620de96d1ec1217ed
Sydney Dell <sadell@mit.edu>
2024-10-30 14:18:29 -0400

    Add /info api to show the number of people in the phonebook and add /api/persons/:id to display the information for a single phonebook entry

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+// Hardcoded list of phonebook entries
 let persons = [
     { 
       "id": "1",
@@ -24,15 +25,43 @@ let persons = [
     }
 ]
 
+// Title of the API
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook API</h1>')
 })
-  
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
-  
+
+// Show that the port is running correctly
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+  
+// Display a hardcoded list of phonebook entries
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+// Show the number of entries in the phonebook and the time the request was made
+app.get('/info', (request, response) => {
+    const numPersons = persons.length
+    const currentTime = new Date()
+
+    response.send(`
+        <p>Phonebook has info for ${numPersons} people</p>
+        <p>${currentTime}</p>
+    `)
+})
+
+// Display the information for a single phonebook entry
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+  
