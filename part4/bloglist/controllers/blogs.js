9ee833b238b09b86cdae6384653e0532907f097c
Sydney Dell <sadell@mit.edu>
2024-11-24 11:44:27 -0500

    completed exercises 4.8-4.12 - wrote unit tests for http methods

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -9,18 +9,19 @@ blogsRouter.get('/', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
     
+    if (body.title === undefined || body.url === undefined) {
+        return response.status(400).end()
+    }
+
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes || 0
     })
 
-    blog.save()
-        .then(savedBlog => {
-            response.json(savedBlog)
-        })
-        .catch(error => next(error))
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -9,27 +9,109 @@ const helper = require('./test_helper')
 const Blog = require('../models/blog')
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
+    await Blog.deleteMany({})
 
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+    for (let blog of helper.initialBlogs) {
+      let blogObject = new Blog(blog)
+      await blogObject.save()
+    }
 })
 
-test('blog info is returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+test('Blog info is returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 })
 
-test('there are two blog posts saved', async () => {
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, 2)
+test('There are two blog posts saved', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, 2)
+})
+
+test('The unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+
+    assert.ok(blogs[0].id, 'Expected property "id" to be defined')
+    assert.strictEqual(blogs[0]._id, undefined, 'Property "_id" should not exist')
+})
+
+test('A valid blog post can be added', async () => {
+    const newBlog = {
+      "title": "A new blog",
+      "author": "John Smith",
+      "url": "https://www.traveler.com/gallery/",
+      "likes": 13
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1, 'Expected blog to be added')
+
+    const titles = blogsAtEnd.map(n => n.title)
+    assert(titles.includes("A new blog"))
+})
+
+test('Likes property defaults to 0 if it is missing', async () => {
+    const newBlog = {
+      "title": "A new blog",
+      "author": "John Smith",
+      "url": "https://www.traveler.com/gallery/"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const addedBlog = await blogsAtEnd.find(blog => blog.title === "A new blog")
+    assert.strictEqual(addedBlog.likes, 0)
+})
+
+test('Blog without title is not added', async () => {
+    const newBlog1 = {
+      "author": "John Smith",
+      "url": "https://www.traveler.com/gallery/",
+      "like": 13
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog1)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test('Blog without url is not added', async () => {
+    const newBlog2 = {
+      "title": "A new blog",
+      "author": "John Smith",
+      "like": 13
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog2)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 })
 
 after(async () => {
-  await mongoose.connection.close()
+    await mongoose.connection.close()
 })
 
+
+
+// verify that if title or url properties are missing from request data, backend respoonds with 400 Bad request
